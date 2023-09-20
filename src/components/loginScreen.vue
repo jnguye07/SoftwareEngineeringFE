0782fed3367da31553e046c3d8309fd2672e76fa
Ivan Welborn <113381087+iwelbo00@users.noreply.github.com>
2023-09-19 20:09:20 -0500

    Working login

--- src/components/loginScreen.vue
+++ src/components/loginScreen.vue
@@ -1,28 +1,37 @@
 <template >
     <v-sheet class="bg-deep-purple pa-12" rounded>
-        <v-card class="mx-auto px-6 py-8" max-width="344">
-            <v-form v-model="form" @submit.prevent="onSubmit">
+        <v-card v-if="showForm" class="mx-auto px-6 py-8" max-width="344">
+            <v-form v-model="form" @submit.prevent="">
                 <h1>User Login</h1>
-                <v-text-field v-model="email" :readonly="loading" :rules="[required]" class="mb-2" clearable
-                    label="Username"></v-text-field>
+                <v-text-field v-model="email" :readonly="loading" class="mb-2" clearable label="Username"></v-text-field>
 
-                <v-text-field v-model="password" :readonly="loading" :rules="[required]" clearable label="Password"
+                <v-text-field type="password" v-model="password" :readonly="loading" clearable label="Password"
                     placeholder="Enter your password"></v-text-field>
 
                 <br>
 
-                <v-btn :disabled="!form" :loading="loading" block color="success" size="large" type="submit"
-                    variant="elevated">
+                <v-btn @click="onSubmit" :disabled="!form" :loading="loading" block color="success" size="large"
+                    type="submit" variant="elevated">
                     Sign In
                 </v-btn>
             </v-form>
         </v-card>
+        <dataTable v-if="!showForm"></dataTable>
     </v-sheet>
 </template>
 
 <script>
+import dataTable from './dataTable.vue'
+
 export default {
+
+    components: {
+        dataTable
+    },
+    props: ['isLoggedIn'],
+
     data: () => ({
+        showForm: true,
         form: false,
         email: null,
         password: null,
@@ -32,18 +41,20 @@ export default {
     }),
 
     methods: {
-        loadIn(){
-            this.users.set("ivan","pass123");
-            this.users.set("justin","pass1234");
+        loadIn() {
+            this.users.set("ivan", "pass123");
+            this.users.set("justin", "pass1234");
         },
         onSubmit() {
             var temp = this.email;
-            if (this.users.has(temp)){
-                if(this.users.get(temp) == this.password){
+            temp = temp.toLowerCase();
+            temp = temp.replace(/\s/g, '');
+            if (this.users.has(temp)) {
+                if (this.users.get(temp) == this.password) {
                     console.log("success");
                     this.email = "";
                     this.password = "";
-                    
+                    this.showForm = false;
                 }
             }
 
@@ -51,11 +62,8 @@ export default {
 
             setTimeout(() => (this.loading = false), 2000)
         },
-        required(v) {
-            return !!v || 'Field is required'
-        },
     },
-    beforeMount(){
+    beforeMount() {
         this.loadIn();
     },
 }
